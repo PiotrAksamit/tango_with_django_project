da8c1afa7923a81270ff44df68205b0172c111f0
“Piotr <2777848a@student.gla.ac.uk>
2025-02-13 13:39:57 +0000

    commit 5

--- rango/views.py
+++ rango/views.py
@@ -13,5 +13,6 @@ def index(request):
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+    context_dict = {}
+    return render(request, 'rango/about.html', context=context_dict)
 
